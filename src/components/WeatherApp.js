b2ad66500920c20a1aa5dec4992c3361139c6156
seba <sebaesains77@gmail.com>
2022-10-20 12:29:12 -0300

    Comentarios en WeatherApp

--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -4,16 +4,18 @@ import WeatherMainInfo from "./WeatherMainInfo";
 import styles from './WeatherApp.module.css';
 import Loading from "../Loading";
 
+// Puede haber tantos useEffect como se necesiten
+
 export default function WeatherApp() {
     const [weather, setWeather] = useState(null);
 
     useEffect(() => {
         loadInfo();
-    }, []);
+    }, []); // cuando este arreglo esta vacio este useEffect se ejecuta solo una vez
 
     useEffect(() => {
         document.title = `Weather: ${weather?.location.name ?? ""}`;
-    }, [weather]);
+    }, [weather]);  // Este useEffect se va a ejecutar solo cuando la variable weather cambie, sino no
 
     async function loadInfo(city = "Rauch") {
         try {
